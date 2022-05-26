8951b40408dfebc09dc507e8db4d74f99eea6f8b
Willi Rath <wrath@geomar.de>
2022-05-26 12:24:19 +0200

    Add Levenshtein metric

--- lagrangian_trajectory_clustering/metrics.py
+++ lagrangian_trajectory_clustering/metrics.py
@@ -70,9 +70,7 @@ def lcs_pure(x, y):
     C = []
     for i in range(m + 1):
         C.append(
-            [
-                0,
-            ]
+            [0,]
         )
     for j in range(1, n + 1):
         C[0].append(0)
@@ -85,5 +83,52 @@ def lcs_pure(x, y):
     return C[m][n]
 
 
+def levenshtein_numpy(x, y):
+    """Calculate the Levenshtein distance between x and y.
+
+    Numpy based-implementation.
+
+    See <https://en.wikipedia.org/wiki/Wagner%E2%80%93Fischer_algorithm>
+
+    Parameters
+    ----------
+    x: iterable
+        First sequence.
+    y: iterable
+        First sequence.
+
+    Even though mixed-type sequences (e.g. [1, 2, "a", "b", True]) will work here, this is not supported as
+    Numba seems to have problems with it.
+
+    Returns
+    -------
+    int
+        Levenshtein distance.
+
+    """
+    m = len(x)
+    n = len(y)
+    d = np.zeros((m + 1, n + 1), dtype="int")
+
+    for i in range(1, m + 1):
+        d[i, 0] = i
+
+    for j in range(1, n + 1):
+        d[0, j] = j
+
+    for j in range(1, n + 1):
+        for i in range(1, m + 1):
+            if x[i - 1] == y[j - 1]:
+                cost = 0
+            else:
+                cost = 1
+
+            d[i, j] = min(d[i - 1, j] + 1, d[i, j - 1] + 1, d[i - 1, j - 1] + cost,)
+
+    return d[m, n]
+
+
 lcs_numpy_numba = jit(lcs_numpy)
 lcs_pure_numba = jit(lcs_pure)
+
+levenshtein_numpy_numba = jit(levenshtein_numpy)
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,19 +1,54 @@
 import pytest
 
+
+from string import ascii_uppercase
+import random
+
+
 from lagrangian_trajectory_clustering.metrics import (
     lcs_numpy,
     lcs_numpy_numba,
     lcs_pure,
     lcs_pure_numba,
+    levenshtein_numpy,
+    levenshtein_numpy_numba,
 )
 
 
 @pytest.mark.parametrize(
-    "lcs_func", [lcs_numpy, lcs_pure, lcs_numpy_numba, lcs_pure_numba]
+    "lcs_implementation", [lcs_numpy, lcs_pure, lcs_numpy_numba, lcs_pure_numba]
 )
-def test_lcs(lcs_func):
-    assert 3 == lcs_func("ABC", "_AB_C_")
-    assert 1 == lcs_func("CBA", "_AB_C_")
-    assert 0 == lcs_func("YYY", "XXX")
-    assert 1 == lcs_func("__Y", "YYY")
-    assert 0 == lcs_func("", "")
+def test_lcs(lcs_implementation):
+    assert 3 == lcs_implementation("ABC", "_AB_C_")
+    assert 1 == lcs_implementation("CBA", "_AB_C_")
+    assert 0 == lcs_implementation("YYY", "XXX")
+    assert 1 == lcs_implementation("__Y", "YYY")
+    assert 0 == lcs_implementation("", "")
+
+
+@pytest.mark.parametrize("lcs_pure_implementation", [lcs_pure, lcs_pure_numba])
+@pytest.mark.parametrize("lcs_numpy_implementation", [lcs_numpy, lcs_numpy_numba])
+def test_lcs_implementations_back_to_back(
+    lcs_pure_implementation, lcs_numpy_implementation
+):
+    """Compare alternative implementations of LCS."""
+    num_runs = 10
+    max_sequence_length = 100
+    for run in range(num_runs):
+        x = "".join(
+            random.choice(ascii_uppercase)
+            for n in range(random.randint(1, max_sequence_length))
+        )
+        y = "".join(
+            random.choice(ascii_uppercase)
+            for n in range(random.randint(1, max_sequence_length))
+        )
+        assert lcs_pure_implementation(x, y) == lcs_numpy_implementation(x, y)
+
+
+@pytest.mark.parametrize(
+    "levenshtein_implementation", [levenshtein_numpy, levenshtein_numpy_numba]
+)
+def test_levenshtein(levenshtein_implementation):
+    assert 3 == levenshtein_implementation("kitten", "sitting")
+    assert 0 == levenshtein_implementation("abcde", "abcde")
